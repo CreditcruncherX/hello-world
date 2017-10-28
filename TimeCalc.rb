74dc9afdcae3e1c0ac55eb69187cb826719244c1
CreditcruncherX <martin.hall@jdzconsult.com>
2017-10-28 20:47:44 +0100

    Add files via upload

---
+++ TimeCalc.rb
@@ -0,0 +1,12 @@
+puts "Time Calcualtor"
+puts ""
+puts "Enter Start Time Hour:-"
+sthour=gets.chomp
+puts "Enter Start Time Minutes:-"
+stmin=gets.chomp
+puts "Start time = H#{sthour}  M#{stmin}"
+puts "Enter Finish Time Hour:-"
+fhour=gets.chomp
+puts "Enter Finish Time Minutes:-"
+fmin=gets.chomp
+puts "Finish time = H#{fhour}  M#{fmin}"
